90973774050597888c7274ad8250a8972d3b16b3
apreble21 <61958972+apreble21@users.noreply.github.com>
2020-08-27 14:48:23 -0600

    html/css/javscript updates, mainly style updates

---
+++ static/JS/script.js
@@ -0,0 +1,199 @@
+$(function () {
+
+	$(window).stellar({
+		horizontalScrolling: false 
+	});
+
+	// Custom Scrollbar
+	var nice = $("html").niceScroll({
+		cursorwidth: 8,
+		cursorborder: "0px solid #fff",
+		cursorborderradius: '0'
+	});
+
+	$('.main-nav a:not(.dropdown-toggle)').bind('click', function(event) {
+		var $anchor = $(this);
+
+		$('html, body').stop().animate({
+			scrollTop: $($anchor.attr('href')).offset().top
+		}, 1500, 'easeInOutExpo');
+
+		event.preventDefault();
+	});
+
+    function home_height () {
+		var element = $('.st-home-unit'),
+			elemHeight = element.height(),
+			winHeight = $(window).height()
+			padding = (winHeight - elemHeight - 200) /2;
+
+		if (padding < 1 ) {
+			padding = 0;
+		};
+		element.css('padding', padding+'px 0');
+	}
+	home_height ();
+
+	$(window).resize(function () {
+		home_height ();
+	});
+
+
+	var fadeStart=$(window).height()/3 // 100px scroll or less will equiv to 1 opacity
+    ,fadeUntil=$(window).height() // 200px scroll or more will equiv to 0 opacity
+    ,fading = $('.st-home-unit')
+    ,fading2 = $('.hero-overlayer')
+	;
+
+	$(window).bind('scroll', function(){
+	    var offset = $(document).scrollTop()
+	        ,opacity=0
+	        ,opacity2=1
+	    ;
+	    if( offset<=fadeStart ){
+	        opacity=1;
+	        opacity2=0;
+	    }else if( offset<=fadeUntil ){
+	        opacity=1-offset/fadeUntil;
+	        opacity2=offset/fadeUntil;
+	    }
+	    fading.css({'opacity': opacity});
+
+	    if (offset >= 120) {
+	    	$('.st-navbar').addClass("st-navbar-mini");
+	    } else if (offset <= 119) {
+	    	$('.st-navbar').removeClass("st-navbar-mini");
+	    }
+	});
+
+	$(".testimonials-carousel ul").owlCarousel({
+        items: 1,
+        navigation: false,
+        pagination: true,
+        singleItem:true,
+        autoPlay: true,
+        navigationText: ['<i class="ct-etp etp-arrow-left7"></i>', '<i class="ct-etp etp-arrow-right8"></i>'],
+        transitionStyle: "backSlide"
+    });
+
+    $('.clients-carousel').owlCarousel({
+    	items: 5,
+    	autoPlay: true,
+    	pagination: false
+    });
+    
+	
+	/*
+     * Contact Form Validation Code
+     */
+    function checkEmpty(selector) {
+        if (selector.val()=="" || selector.val()==selector.prop("placeholder")) {
+          selector.addClass('formFieldError',500);
+          return false;
+        } else {
+          selector.removeClass('formFieldError',500); 
+          return true;
+        }
+    }
+    $('.form-inline').submit(function () {
+      var $this = $(this),
+          result = true;
+
+      if(!checkEmpty($this.find('#genderSelect'))){
+        result=false;
+      }
+      if(!checkEmpty($this.find('#ethnicitySelect'))){
+        result=false;
+      }
+      if(!checkEmpty($this.find('#selectLocation'))){
+        result=false;
+      }
+      if(!checkEmpty($this.find('#selectEducation'))){
+        result=false;
+      }
+      if(!checkEmpty($this.find('#maritalSelect'))){
+        result=false;
+      }
+      if(!checkEmpty($this.find('#selectChocolate'))){
+        result=false;
+      }
+      if(!checkEmpty($this.find('#selectCheeese'))){
+        result=false;
+      }
+      if(!checkEmpty($this.find('#selectAlcohol'))){
+        result=false;
+      }
+      if(!checkEmpty($this.find('#selectCoffee'))){
+        result=false;
+      }
+      if(!checkEmpty($this.find('#selectCandy'))){
+        result=false;
+      }
+      if(!checkEmpty($this.find('#selectScent'))){
+        result=false;
+      }
+      if(!checkEmpty($this.find('#selectDressing'))){
+        result=false;
+      }
+      if(!checkEmpty($this.find('#ageInput'))){
+        result=false;
+      }
+      if(result==false) {
+        return false;
+      }
+
+      var $btn = $("#submit-form").button('loading');
+
+      var data = $this.serialize();
+
+      $.ajax({
+          url: "sender.php", 
+          type: "POST",        
+          data: data,     
+          cache: false,
+          success: function (html) {
+          	console.log(html);
+              if (html==1) {
+                  $('#result-message').addClass('alert alert-success').html('<button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button><strong>Success!</strong> Message Send. We will contact with you soon.').delay(500).slideDown(500).delay(10000).slideUp('slow');
+
+                  $btn.button('reset');
+                  
+              } else {
+                  $('#result-message').addClass('alert alert-danger').html('<button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button><strong>Error!</strong> Message Sending Error! Please try again').delay(500).slideDown(500).delay(10000).slideUp('slow');
+                  $btn.button('reset');
+              }
+          },
+          error: function (a, b) {
+            if (b == 'error') {
+              $('#result-message').addClass('alert alert-danger').html('<button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button><strong>Error!</strong> Message Sending Error! Please try again').delay(500).slideDown(500).delay(10000).slideUp('slow');
+            };
+            $btn.button('reset');
+          }
+      });
+
+      return false;
+    });
+
+
+});
+
+
+$(window).load(function () {
+	var $grid = $('.grid'),
+		$sizer = $grid.find('.shuffle__sizer'),
+		$filterType = $('#filter input[name="filter"]');
+
+	$grid.shuffle({
+		itemSelector: '.portfolio-item',
+		sizer: $sizer
+	});
+
+	$filterType.change(function(e) {
+		var group = $('#filter input[name="filter"]:checked').val();
+
+		$grid.shuffle('shuffle', group);
+
+		$('label.btn-main').removeClass('btn-main');
+		$('input[name="filter"]:checked').parent().addClass('btn-main');
+	});
+});
